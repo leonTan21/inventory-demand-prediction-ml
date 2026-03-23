9fcfe4d7f7a3d956fcf7e9fc167b958870f7b4e8
Lin En Tan <linentan04@gmail.com>
2026-03-23 10:30:54 -0500

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,21 +1,27 @@
-from sklearn.datasets import load_iris
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from data_loader import load_walmart_data
 
+#Load dataset
 df = load_walmart_data()
+print("Dataset preview:")
 print(df.head())
+print("\nColumns:", df.columns)
 
-# Load dataset
-data = load_iris()
-X, y = data.data, data.target
+#Convert date to datetime
+if 'Date' in df.columns:
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst = True)
+    df['Year'] = df['Date'].dt.year
+    df['Month'] = df['Date'].dt.month
+    df['Day'] = df['Date'].dt.day
+    df.drop(columns=['Date'], inplace=True)
 
-# Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#Define features (X) and target (y)
+target = 'Weekly_Sales'
+X = df.drop(columns=[target])
+y = df[target]
 
-# Train
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-# Evaluate
-print("Accuracy:", model.score(X_test, y_test))
+#Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
